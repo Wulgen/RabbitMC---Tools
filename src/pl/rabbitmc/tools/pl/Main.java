6949fd568835cc362f710c188af097a87741111c
Wulgen <maciekgames17333@gmail.com>
2018-02-25 23:15:55 +0100

    Version: 0.0
    Powiadomienie w konsoli czy plugin sie uruchomił czy nie

--- src/pl/rabbitmc/tools/pl/Main.java
+++ src/pl/rabbitmc/tools/pl/Main.java
@@ -1,29 +1,18 @@
 package pl.rabbitmc.tools.pl;
 
-import org.bukkit.command.Command;
-import org.bukkit.entity.Player;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-
 public class Main{
-	
+
+
 	public void onEnable(){ 
-	    System.out.PrintIn("RabbitMC.pl > Uruchamianie!");
+	    System.out.println("RabbitMC.pl > Uruchamianie!");
 	}
 
 	public void onDisable() {
-		System.out.PrintIn("RabbitMC.pl > Wylaczanie"); 
+		System.out.println("RabbitMC.pl > Wylaczanie"); 
 	}
+} 
 
-	public boolean onCommand(CommandSender sender, Command cmd, String label , String[] args){ 
-		if(cmd.getName().equalsIgnoreCase("ranga"))
-			if(Sender instanceof Player){
-				Player p = (Player) sender;
-				p.sendMessage("Twoja ranga to:");{
-		}
-	}
-}
-
+					
 			
 		
 
